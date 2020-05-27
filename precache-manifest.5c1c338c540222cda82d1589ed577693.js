1804dcf0e436fb920ed20055ce77656a11da5a56
Morrism1 <morrismontana0@gmail.com>
2020-05-27 06:29:28 +0200

    Updates

---
+++ precache-manifest.5c1c338c540222cda82d1589ed577693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a36f36420ab973def03c0de86f036b",
+    "url": "/roledex-robot/index.html"
+  },
+  {
+    "revision": "f44f2dd298b7b854db7d",
+    "url": "/roledex-robot/static/css/main.741e6fef.chunk.css"
+  },
+  {
+    "revision": "78f1f8c327a3ffa553c1",
+    "url": "/roledex-robot/static/js/2.bc314da5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roledex-robot/static/js/2.bc314da5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44f2dd298b7b854db7d",
+    "url": "/roledex-robot/static/js/main.1d3ef9f8.chunk.js"
+  },
+  {
+    "revision": "bb058b975eb857af36f7",
+    "url": "/roledex-robot/static/js/runtime-main.85ef7937.js"
+  }
+]);
